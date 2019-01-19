17a1ef8342d6a7018d6e356d8a21e62f78fd6e41
robin <robin@ansvia.com>
2019-01-20 01:21:45 +0700

    bump update

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,23 +1,13 @@
-
-use std::{
-    env,
-    time::Duration,
-    sync::Arc,
-    thread::{self, sleep}
-};
+use std::{env, sync::Arc, thread::sleep, time::Duration};
 
 extern crate event_stream;
 
-use event_stream::{
-    EventDispatcherBuilder,
-    EventListener
-};
-
+use event_stream::{EventDispatcherBuilder, EventListener};
 
 #[derive(Debug)]
 enum Event {
     Ping,
-    Hello(String)
+    Hello(String),
 }
 
 struct MyListener;
@@ -29,7 +19,7 @@ impl MyListener {
 }
 
 impl EventListener<Event> for MyListener {
-    fn dispatch(&self, event: &Event){
+    fn dispatch(&self, event: &Event) {
         println!("{:?} got event: {:?}", self, event);
     }
 }
@@ -40,9 +30,7 @@ impl std::fmt::Debug for MyListener {
     }
 }
 
-
 fn main() {
-
     env::set_var("RUST_LOG", "simple=debug");
     env::set_var("RUST_BACKTRACE", "1");
 
@@ -53,7 +41,7 @@ fn main() {
     sleep(Duration::from_secs(1));
 
     event_dispatcher.emit(Event::Ping);
-    
+
     sleep(Duration::from_secs(1));
 
     event_dispatcher.emit(Event::Hello("world".to_string()));
@@ -62,5 +50,3 @@ fn main() {
 
     println!("done.");
 }
-
-
--- src/lib.rs
+++ src/lib.rs
@@ -1,89 +1,85 @@
-
 #[macro_use]
 extern crate crossbeam_channel;
 
-use crossbeam_channel::{unbounded, Sender, Receiver};
+use crossbeam_channel::{unbounded, Receiver, Sender};
 
-use std::{
-    thread,
-    sync::Arc
-};
+use std::{sync::Arc, thread};
 
-pub trait EventListener<T> 
-where 
-    T: Sync + Send + 'static
+pub trait EventListener<T>
+where
+    T: Sync + Send,
 {
     fn dispatch(&self, event: &T);
 }
 
-type EL<T> = dyn EventListener<T> + Send + Sync;
-
 pub struct EventDispatcher<T> {
     tx: Sender<T>,
     rx: Receiver<T>,
-    listeners: Vec<Arc<EL<T>>>
+    listeners: Vec<Arc<dyn EventListener<T> + Send + Sync>>,
 }
 
 pub struct EventDispatcherBuilder<T> {
     tx: Sender<T>,
     rx: Receiver<T>,
-    listeners: Vec<Arc<EL<T>>>
+    listeners: Vec<Arc<dyn EventListener<T> + Send + Sync>>,
 }
 
 impl<T> EventDispatcherBuilder<T>
-where 
-    T: Sync + Send + 'static
+where
+    T: Sync + Send + Sized,
 {
     pub fn new() -> Self {
         let (tx, rx) = unbounded::<T>();
-        EventDispatcherBuilder { 
-            tx, rx,
-            listeners: vec![] 
+        EventDispatcherBuilder {
+            tx,
+            rx,
+            listeners: vec![],
         }
     }
 
-    pub fn add_listener(&mut self, listener: Arc<EL<T>>) -> &mut Self {
+    pub fn add_listener(mut self, listener: Arc<dyn EventListener<T> + Send + Sync>) -> Self {
         self.listeners.push(listener);
         self
     }
 
-    pub fn build(&self) -> Arc<EventDispatcher<T>> {
-        Arc::new(EventDispatcher {
-            tx: self.tx.clone(), 
+    pub fn build(self) -> EventDispatcher<T> {
+        EventDispatcher {
+            tx: self.tx.clone(),
             rx: self.rx.clone(),
-            listeners: self.listeners.clone()
-        }.start())
+            listeners: self.listeners,
+        }
     }
-
 }
 
-impl<T> EventDispatcher<T> 
+impl<T> EventDispatcher<T>
 where
-    T: Sync + Send + 'static
+    T: Sized + Send + Sync + 'static,
 {
-    pub fn emit(&self, event: T){
-        match self.tx.send(event){
+    pub fn emit(&self, event: T) {
+        match self.tx.send(event) {
             Err(e) => println!("error: {}", e),
-            _ => ()
+            _ => (),
         }
     }
 
-    pub fn start(self) -> Self {
+    pub fn start(&self) {
         let rx_thread = self.rx.clone();
         let listeners = self.listeners.clone();
-        thread::spawn(move || {
-            loop {
-                select! {
-                    recv(rx_thread) -> msg => {
-                        for listener in &listeners {
-                            if let Ok(event) = &msg {
-                                listener.dispatch(&event);
-                            }
+
+        thread::spawn(move || loop {
+            select! {
+                recv(rx_thread) -> msg => {
+                    for listener in listeners.iter() {
+                        if let Ok(event) = &msg {
+                            listener.dispatch(&event);
                         }
                     }
                 }
             }
         });
-        self
+    }
+
+    pub fn get_channel(&self) -> &Sender<T> {
+        &self.tx
     }
 }
